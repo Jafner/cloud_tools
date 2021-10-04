c7881d95b869ddae47f585c48656cc838d95d55b
Jafner <40403594+Jafner@users.noreply.github.com>
2021-10-03 19:22:13 -0700

    Improve ugly mkdir

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker network create web
 sed -i "s/email = \"\"/email = \"$EMAIL\"/g" traefik.toml
-mkdir ./htdocs ./htdocs/download
+mkdir -p ./htdocs/download
 touch acme.json 
 chmod 600 acme.json
 docker-compose up -d
